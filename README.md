bbc875cff898f27cee25bed92cc36f7c52c3e3c5
Vlad Sizion <vladislav.sizion@mail.ru>
2019-09-29 17:09:10 +0300

    Added class - HelloOtus and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Java-in-my-heart
+Vladislav Sizion 
+
+Group: Java-SE-2019-09
---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,12 @@
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+
+        Multiset< String > wordsMultiset = HashMultiset.create();
+        System.out.println("Done");
+
+    }
+}
